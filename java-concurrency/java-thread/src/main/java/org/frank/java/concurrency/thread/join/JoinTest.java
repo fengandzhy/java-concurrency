ecbf2a86c48240b0aab667eca7e6479f7fa9a386
fengandzhy <fengandzhy@gmail.com>
2023-11-26 21:37:33 +1300

    join

--- java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/join/JoinTest.java
+++ java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/join/JoinTest.java
@@ -1,24 +1,26 @@
 package org.frank.java.concurrency.thread.join;
 
-import java.util.concurrent.atomic.AtomicInteger;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "c.JoinTest")
 public class JoinTest {
     private static int result = 0;
     public static void main(String[] args) throws InterruptedException {        
         Thread t1 = new Thread(()-> {
-            System.out.println("Thread is running.");
+            log.debug("Thread is running.");
             try {
                 Thread.sleep(5000);
                 result = 2;
-                System.out.println("Thread is finished.");
+                log.debug("Thread is finished.");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         });
         t1.setName("t1");
         t1.start();
-        System.out.println(result);
-        t1.join();
-        System.out.println(result);
+        log.debug("before join result=" + result);
+        t1.join(); // 在这里等待t1 完成
+        log.debug("after join result=" + result);
     }   
 }

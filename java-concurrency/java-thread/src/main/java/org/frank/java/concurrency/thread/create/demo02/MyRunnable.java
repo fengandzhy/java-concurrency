8640ecbfcc82a66fc5766f404554a0ad54a6874c
fengandzhy <fengandzhy@gmail.com>
2023-11-26 15:32:00 +1300

    pom file

---
+++ java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/create/demo02/MyRunnable.java
@@ -0,0 +1,8 @@
+package org.frank.java.concurrency.thread.create.demo02;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("This is my Runnable.");
+    }
+}
--- java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/create/demo02/ThreadCreation2.java
+++ java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/create/demo02/ThreadCreation2.java
@@ -5,15 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j(topic = "c.ThreadCreation2")
 public class ThreadCreation2 {
     public static void main(String[] args) {
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                log.info(" is running");
-            }
-        };
-        
-        Thread thread = new Thread(runnable);
-        thread.setName("t2");
-        thread.start();        
+        Runnable runnable = new MyRunnable();        
+        Thread t2 = new Thread(runnable);
+        t2.setName("t2");
+        t2.start();        
     }
 }

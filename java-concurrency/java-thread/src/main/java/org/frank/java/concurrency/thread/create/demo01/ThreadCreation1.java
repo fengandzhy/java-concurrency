643b8b8ccf9282c02cce32c95f79fd8f936bfe80
fengandzhy <fengandzhy@gmail.com>
2023-11-26 15:30:28 +1300

    pom file

--- java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/create/demo01/ThreadCreation1.java
+++ java-concurrency/java-thread/src/main/java/org/frank/java/concurrency/thread/create/demo01/ThreadCreation1.java
@@ -2,12 +2,13 @@ package org.frank.java.concurrency.thread.create.demo01;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 
+ * */
 @Slf4j(topic = "c.ThreadCreation1")
 public class ThreadCreation1 {
     public static void main(String[] args) {
-        Thread t1 = new Thread(() ->{
-            log.info(" is running");
-        });
+        Thread t1 = new MyThread();
         t1.setName("t1");
         t1.start();
     }    
